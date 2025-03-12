0dce3c7f46a6deaa793765567fdc6ee971a23bbb
Victor <victor.gitonga1@student.moringaschool.com>
2025-03-12 08:58:16 +0300

    phase 1 proj 1

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+const companyName = 'Scuber'
+let mostProfitableNeighborhood = 'Chelsea'
+let companyCeo = 'Susan Smith'
+
+function ageInDays(ageInYears) {
+    const daysInYear = 365;
+    return ageInYears * daysInYear;
+}
+const age = 30;
+console.log(ageInDays(30));  //Output: 10,950
+
+function reverseArray(inputArray) {
+    return inputArray.slice ().reverse(); 
+}
+ console.log(reverseArray(1, 2, 3, 4, 5));
+
+function isEvenOrOdd(2) {
+    if (number % 2 === 0) {
+        return 'Even';
+    } else {
+        return 'Odd';
+    }
+}
+console.log(isEvenOrOdd(4));  // Output: Even
+console.log(isEvenOrOdd(7));  // Output: Odd
+
+function ageInDays(ageInYears) {
+    const daysInYear = 365;
+    return ageInYears*daysInYear;
+}
+console.log(ageInDays(30));
+
--- test/indexTest.js
+++ test/indexTest.js
@@ -7,7 +7,7 @@ const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
 
 describe('index.js', function () {
   describe('companyName', function () {
-    it('is set as Scuber', function () {
+    it('is set as Scuber', funcion ()t {
       expect(companyName).to.equal('Scuber');
     });
 
